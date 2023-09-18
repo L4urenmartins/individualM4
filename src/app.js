7b360cbd132914cd90a9f303e88425ed32e6ccba
L4urenmartins <lauren_martins_borba@hotmail.com>
2023-09-18 19:20:25 -0300

    feat: Tratamento de erro de entrada

--- src/app.js
+++ src/app.js
@@ -1,20 +1,26 @@
 import redline from "readline-sync";
 
-const cssPropriedades = [];
+
 
 function redlinePrompt() {
 
+    const cssPropriedades = [];
     let propriedade = (redline.question("Digite uma propriedade de CSS ou sair para encerrar: ")).toUpperCase();
-    if (propriedade !== "SAIR") {
+    const naoENumero = isNaN(parseInt(propriedade));
+    if (propriedade.trim() === '' || !naoENumero) {
+        console.log('Entrada vazia ou inválida. Por favor, insira uma propriedade válida ou digite "SAIR".');
+        redlinePrompt();
+    } else if (propriedade !== "SAIR") {
         cssPropriedades.push(propriedade);
         redlinePrompt();
+
     } else {
         cssPropriedades.sort();
         cssPropriedades.forEach((cssPropriedade) => {
             console.log(cssPropriedade);
         })
     }
-
 }
 
-redlinePrompt();
+
+redlinePrompt()
