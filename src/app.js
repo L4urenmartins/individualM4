8fa5e8864e81cb592fcc94060c4b5452424ff7d1
L4urenmartins <lauren_martins_borba@hotmail.com>
2023-09-18 18:57:50 -0300

    feat: organizado o projeto em pastas para melhorar a compreensão

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ function redlinePrompt() {
         redlinePrompt();
     } else {
         cssPropriedades.sort();
-        cssPropriedades.forEach((cssPropriedade)=>{
+        cssPropriedades.forEach((cssPropriedade) => {
             console.log(cssPropriedade);
         })
     }
